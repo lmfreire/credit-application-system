4720b84c814e9920a1e2282af0b5815b0eb4d340
lmFreire <l.matheus.freire10@gmail.com>
2024-11-21 20:20:26 -0300

    anotações JPA usando jakar

--- src/main/kotlin/me/dio/credit/applicatio/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/applicatio/system/entity/Customer.kt
@@ -1,12 +1,21 @@
 package me.dio.credit.applicatio.system.entity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Cliente")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String = "",
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
-    var credits: List<Credit> = mutableListOf(),
-    val id: Long? = null
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String = "",
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(
+        fetch = FetchType.LAZY,
+        cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
+        mappedBy = "customer",
+        targetEntity = Credit::class
+    ) var credits: List<Credit> = mutableListOf(),
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
